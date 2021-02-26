3d57a3d1fbdb72227bdda83317830bd7179d569b
CaioKeidi <caiokeidi@gmail.com>
2021-02-26 10:56:23 -0300

    feat: UseState on FormularioCadastro

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, TextField, Switch, FormControlLabel} from '@material-ui/core'
 
 function FormularioCadastro(){
+    const [nome, setNome] = useState('');
+    const [sobrenome, SetSobrenome] = useState('');
+
     return(
-        <form>
-            <TextField id='Nome' label='Nome' variant='outlined' margin='normal' fullWidth/>
-            <TextField id='Sobrenome' label='Sobrenome' variant='outlined' margin='normal' fullWidth/>
-            <TextField id='CPF' label='CPF' variant='outlined' margin='normal' fullWidth/>
+        <form onSubmit={ (event) => {
+                event.preventDefault();
+                console.log(nome, sobrenome);
+        }}>
+            <TextField 
+                value = {nome}
+                onChange={event => {
+
+                    let tempNome = event.target.value;
+                    if(nome.length >= 3){
+                        tempNome = tempNome.substr(0, 3);
+                    }
+                    setNome(tempNome)
+                    
+                }}
+                id='Nome' 
+                label='Nome' 
+                variant='outlined' 
+                margin='normal' 
+                fullWidth
+            />
+            <TextField 
+                value = {sobrenome}
+                onChange = {event => {
+                    SetSobrenome(event.target.value)
+                }}
+                id='Sobrenome' 
+                label='Sobrenome' 
+                variant='outlined'
+                margin='normal' 
+                fullWidth
+            />
+            <TextField 
+                id='CPF' 
+                label='CPF' 
+                variant='outlined' 
+                margin='normal' 
+                fullWidth
+            />
 
             <FormControlLabel 
                 label='Promoções'
