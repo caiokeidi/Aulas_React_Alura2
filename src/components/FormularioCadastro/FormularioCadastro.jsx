14c10c0c0cfe6000141d88aad920219ade3ad6cb
CaioKeidi <caiokeidi@gmail.com>
2021-02-28 12:19:55 -0300

    feat: DadosUsuarios and DadosPessoais

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,95 +1,16 @@
 import React, {useState} from 'react';
-import {Button, TextField, Switch, FormControlLabel} from '@material-ui/core'
+import DadosPessoais from './DadosPessoais';
+import DadosUsuario from './DadosUsuario';
 
 
 function FormularioCadastro({aoEnviar, validarCPF}){ //Pegando a propriedade enviada.
-    const [nome, setNome] = useState('');
-    const [sobrenome, SetSobrenome] = useState('');
-    const [cpf, setCpf] = useState('');
-    const [promocoes, setPromocoes] = useState(true);
-    const [novidades, setNovidades] = useState(true);
-    const [erros, setErros] = useState({cpf:{valido:true, texto:''}});
 
     return(
-        <form onSubmit={ (event) => {
-                event.preventDefault();
-                aoEnviar({nome, sobrenome, cpf, novidades, promocoes})
-
-        }}>
-            <TextField 
-                value = {nome}
-                onChange={event => {
-                    setNome(event.target.value)
-                }}
-                id='Nome' 
-                label='Nome' 
-                variant='outlined' 
-                margin='normal' 
-                fullWidth
-            />
-            <TextField 
-                value = {sobrenome}
-                onChange = {event => {
-                    SetSobrenome(event.target.value)
-                }}
-                id='Sobrenome' 
-                label='Sobrenome' 
-                variant='outlined'
-                margin='normal' 
-                fullWidth
-            />
-            <TextField 
-                value= {cpf}
-                onChange = {event => {
-                    setCpf(event.target.value)
-                }}
-
-                onBlur = {event => {
-                    const ehvalido = validarCPF(cpf); //Peguei do estado, mas pode ser event.target.value
-                    setErros({cpf:ehvalido})
-                }}
-                 
-                error={!erros.cpf.valido}
-                helperText={erros.cpf.texto}
-                id='CPF' 
-                label='CPF' 
-                variant='outlined' 
-                margin='normal' 
-                fullWidth
-            />
-
-            <FormControlLabel 
-                label='Promoções'
-                control={<Switch 
-                    checked={promocoes}
-                    onChange = {event => {
-                        setPromocoes(event.target.checked) //Switch tem checked e não value
-                    }}
-                    name='promocoes' 
-                    color='primary'
-                    />}
-            />
-
-            <FormControlLabel
-                checked={novidades}
-                label='Novidades'
-                control={<Switch 
-                    onChange = {event => {
-                        setNovidades(event.target.checked)
-                    }}
-                    name='novidades' 
-                    
-                    color='primary'
-                    />}
-            />
-            
-            
-            <Button type='submit' color="primary" variant="contained">
-                Cadastrar
-            </Button>
-            
-        </form>
-    );
+        <>
+        <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
+        <DadosUsuario/>
+        </>
+    )
 }
 
 export default FormularioCadastro;
