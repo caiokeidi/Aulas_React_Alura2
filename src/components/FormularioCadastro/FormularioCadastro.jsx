a519cda5ade066c519023e5e4ef6e3a549e39487
CaioKeidi <caiokeidi@gmail.com>
2021-02-28 13:04:08 -0300

    feat: add swtich function to FormularioCadastro

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,18 +1,28 @@
-import React, {useState} from 'react';
-import DadosEntrega from './DadosEntrega';
-import DadosPessoais from './DadosPessoais';
-import DadosUsuario from './DadosUsuario';
+import React, { useState } from "react";
+import DadosEntrega from "./DadosEntrega";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
+import { Typography } from "@material-ui/core";
 
+function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(1);
 
-function FormularioCadastro({aoEnviar, validarCPF}){ //Pegando a propriedade enviada.
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario />;
 
-    return(
-        <>
-        <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-        <DadosUsuario/>
-        <DadosEntrega/>
-        </>
-    )
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />;
+
+      case 2:
+        return <DadosEntrega />;
+
+      default:
+        return <Typography>Erro ao selecionar formulário</Typography>;
+    }
+  }
+  return <>{formularioAtual(etapaAtual)}</>;
 }
 
 export default FormularioCadastro;
